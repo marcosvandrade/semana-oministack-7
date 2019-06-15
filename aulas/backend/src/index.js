5187d8d1321b9cf39ee6462a11fa915974b11d7c
marcosvandrade <vascorj2006@gmail.com>
2019-06-15 12:21:23 -0300

    CommitHome15Jun19

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0-fqk19.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true,});
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
